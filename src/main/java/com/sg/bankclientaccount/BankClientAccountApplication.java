a609d7c3266b852de65a014fd162d3a0569d1b5a
PC perso pato <alaaeddine.zammel@esprit.tn>
2022-05-06 14:33:23 +0200

    Initial commit

---
+++ src/main/java/com/sg/bankclientaccount/BankClientAccountApplication.java
@@ -0,0 +1,13 @@
+package com.sg.bankclientaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankClientAccountApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankClientAccountApplication.class, args);
+  }
+
+}
